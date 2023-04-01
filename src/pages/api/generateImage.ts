fad811882d1f9baf49418c274c7ab2e0b4e8b8f5
ThallesP <contato@thalles.me>
2023-04-01 00:47:27 -0300

    feat: generate images with openai

---
+++ src/pages/api/generateImage.ts
@@ -0,0 +1,36 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { PrismaClient } from '@prisma/client';
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { Configuration, OpenAIApi } from "openai";
+
+type Data = {
+  prompt: string,
+  numImages: number,
+  address: string
+}
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const { address, numImages,prompt } = req.body as Data;
+
+  const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+  });
+
+  const openai = new OpenAIApi(configuration);
+  const prisma = new PrismaClient();
+
+  const response = await openai.createImage({
+    prompt: prompt,
+    n: numImages,
+    size: '512x512',
+  });
+
+  await prisma.userImage.createMany({
+    data: response.data.data.map((image) => ({ image_url: image.url || '', address: address, prompt }))
+  });
+
+  return res.json(response.data.data)
+}
---
+++ src/pages/api/getImagesByAddress.ts
@@ -0,0 +1,16 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { PrismaClient } from '@prisma/client'
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const address = req.query.address as string
+
+  const prisma = new PrismaClient()
+
+  const images = await prisma.userImage.findMany({ where: { address } })
+
+  return res.json(images)
+}

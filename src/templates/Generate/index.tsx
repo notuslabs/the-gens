7f71b925fbcb6b8ce4993452069987fd3be8120a
ThallesP <contato@thalles.me>
2023-04-01 16:40:50 -0300

    merge

--- src/templates/Generate/index.tsx
+++ src/templates/Generate/index.tsx
@@ -1,58 +1,35 @@
-import {
-  deleteImage,
-  favoriteImage,
-  generateImage,
-  getImages,
-  mintImage
-} from '@/client'
+import { getImages, Image } from '@/client'
 import React from 'react'
 
 import Cards from '@/components/Cards'
+import GenerateButtons from '@/components/GenerateButtons'
 
 const Generate = () => {
+  const [images, setImages] = React.useState<Image[]>([])
+
   React.useEffect(() => {
-    async function test() {
-      const imageId = '714f333f-8995-423d-8bd0-bec4030ec80b'
-      const address = '0x00'
-
-      /*const generatedImages = await generateImage({
-        address,
-        prompt: 'test',
-        numImages: 1
-      })*/
-
-      /*await mintImage({
-        imageId,
-        minted: true
-      })
-      await favoriteImage({
-        imageId,
-        favorite: true
-      })
-
-      await deleteImage({
-        imageId
-      })*/
-
+    async function generateImage() {
       const images = await getImages({
         address: '0x00',
         filters: {
           onlyMinted: true
         }
       })
-      console.log(images)
+
+      setImages(images)
     }
 
-    test()
+    generateImage()
   }, [])
 
   return (
     <main className="bg-black w-full h-screen columns-2">
       <div className="bg-gray-600 h-screen">
+        <GenerateButtons onClick={console.log} />
         <Cards />
       </div>
       <div className="bg-gray-600 h-screen">
-        <p className="text-white">grid</p>
+        <p className="text-white"></p>
       </div>
     </main>
   )

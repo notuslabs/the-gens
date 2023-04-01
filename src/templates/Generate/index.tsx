e0754bdbcdab97b011dfea8ebace5f9febb25c06
Jony <jonyreiscardoso@gmail.com>
2023-04-01 15:53:04 -0300

    feat: add Cards component

--- src/templates/Generate/index.tsx
+++ src/templates/Generate/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 
+import Cards from '@/components/Cards'
+
 const Generate = () => {
   return (
     <main className="bg-black w-full h-screen columns-2">
       <div className="bg-gray-600 h-screen">
-        <p className="text-white">grid</p>
+        <Cards />
       </div>
       <div className="bg-gray-600 h-screen">
         <p className="text-white">grid</p>

614aa5cf920d6eff11b7b81edc60f7c92a3e53fb
Jony <jonyreiscardoso@gmail.com>
2023-04-01 20:28:22 -0300

    feat: add mint page

--- src/templates/Mint/index.tsx
+++ src/templates/Mint/index.tsx
@@ -1,9 +1,34 @@
 import React from 'react'
 
+import Header from '@/components/Header'
+
 const Mint = () => {
   return (
-    <main className="bg-black w-full h-screen">
-      <p className="text-white">Mint</p>
+    <main className="bg-black w-full h-screen bg-[url('/icons/bg-the-gens.png')] text-white">
+      <Header />
+      <p className="px-8 pt-6 text-white">
+        Uma coleção colaborativa de arte com IA
+      </p>
+      <div className="px-8 pt-48">
+        <p className="w-[320px]">
+          Você pode gastar um Mint Pass para mintar sua criação na coleção THE
+          GENS
+        </p>
+        <button
+          type="button"
+          className="flex rounded-xl font-semibold p-3 my-8  text-black text-base bg-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+          id="user-menu-button"
+          aria-expanded="false"
+          aria-haspopup="true"
+          // onClick={() => connect()}
+        >
+          Compre um Mint Pass
+        </button>
+        <div className="grid grid-cols-1">
+          <span className="mb-1">100 AURORA</span>
+          <span>100/1000 Mint Pass</span>
+        </div>
+      </div>
     </main>
   )
 }

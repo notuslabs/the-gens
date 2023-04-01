0f4f47175913ca7b260cd0a25798f387228ee77f
Jony <jonyreiscardoso@gmail.com>
2023-04-01 16:18:38 -0300

    refactor: update Cadrs compoent

--- src/components/Cards/index.tsx
+++ src/components/Cards/index.tsx
@@ -2,15 +2,19 @@ import React from 'react'
 
 const Cards = () => {
   return (
-    <a href="/" className="relative">
-      <img
-        className="w-80 h-72 border border-solid rounded-lg border-slate-500"
-        src="https://res.cloudinary.com/dmztvy5k7/image/upload/v1680373568/oyui306ouvf3ymswno7g.png"
-        alt=""
-        srcSet=""
-      />
-      <button className="absolute">ADD</button>
-    </a>
+    <div className="w-80 h-72 relative">
+      <a href="/">
+        <img
+          className="w-80 h-72 border border-solid rounded-lg border-slate-500"
+          src="https://res.cloudinary.com/dmztvy5k7/image/upload/v1680373568/oyui306ouvf3ymswno7g.png"
+          alt=""
+          srcSet=""
+        />
+        <button className="absolute right-2 top-2 bg-white rounded-lg">
+          ADD
+        </button>
+      </a>
+    </div>
   )
 }
 

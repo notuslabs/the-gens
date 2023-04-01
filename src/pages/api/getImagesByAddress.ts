2beb42167b9d91907945a4f0ed456d45800bbc36
ThallesP <contato@thalles.me>
2023-04-01 14:32:08 -0300

    feat: add filters to get image by address

--- src/pages/api/getImagesByAddress.ts
+++ src/pages/api/getImagesByAddress.ts
@@ -8,10 +8,21 @@ export default async function handler(
 ) {
   // eslint-disable-next-line prettier/prettier
   const address = req.query.address as string
+  const filter = req.query.filter as string | undefined;
+
+  
+  let finalFilters: any = {}
+
+  if(filter) {
+    const filtersSplit = filter.split(",")
+
+    if(filtersSplit.includes('isFavorited')) finalFilters['isFavorited'] = true
+    if(filtersSplit.includes('isMinted')) finalFilters['isMinted'] = true
+  }
 
   const prisma = new PrismaClient()
 
-  const images = await prisma.userImage.findMany({ where: { address } })
+  const images = await prisma.userImage.findMany({ where: { address, ...finalFilters } })
 
   return res.json(images)
 };

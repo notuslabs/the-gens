bae7e262750823d627ce22461b8714784c19edb0
Jony <jonyreiscardoso@gmail.com>
2023-03-31 23:00:22 -0300

    feat: add Header component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,7 +15,9 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        <h1>ArtInt</h1>
+        <h1 className="text-3xl font-bold underline">
+          Hello world!
+        </h1>
       </main>
     </>
   )

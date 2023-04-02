4bb4a6e60401f5aa9f251b54bf3869f2af5c246e
ChristanDaniel <christian_daniel04@hotmail.com>
2023-04-02 06:26:44 -0400

    add diable in modalViewImage

--- src/components/ModalViewImage/index.tsx
+++ src/components/ModalViewImage/index.tsx
@@ -120,7 +120,7 @@ const ModalViewImage = ({
             <div className="flex gap-2 mt-8">
               <button
                 type="button"
-                // disabled={balanceOf === '0'}
+                disabled={balanceOf === '0'}
                 className="rounded-lg font-semibold p-3 text-base disabled:opacity-75 bg-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
                 onClick={() => handleMint()}
               >

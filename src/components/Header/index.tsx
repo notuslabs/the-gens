424c8fe6409111f7a64e49fbab4933db584701a5
Jony <jonyreiscardoso@gmail.com>
2023-04-02 10:01:22 -0300

    refactor: update link aurora on tofunft

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -60,7 +60,7 @@ const Header = () => {
           Gerar
         </Link>
         <Link
-          href="https://tofunft.com/nft/aurora/0xe273F57f6Bda1b26A8f0cd1dA7ADd65B3850257F"
+          href="https://tofunft.com/aurora"
           target="_blank"
           className="text-neutral-500 font-semibold text-sm cursor-pointer"
         >
